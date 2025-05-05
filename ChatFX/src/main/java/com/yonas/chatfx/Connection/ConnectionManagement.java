7352cec184ec1021d386f5f2f5e915011affb491
yonas790 <yoni2752@gmail.com>
2025-05-05 15:29:15 +0300

    optimizing import method for jdbc

--- ChatFX/src/main/java/com/yonas/chatfx/Connection/ConnectionManagement.java
+++ ChatFX/src/main/java/com/yonas/chatfx/Connection/ConnectionManagement.java
@@ -1,8 +1,6 @@
 package com.yonas.chatfx.Connection;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class ConnectionManagement {
 
