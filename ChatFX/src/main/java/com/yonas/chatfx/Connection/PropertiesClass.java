20275b045751a63583820afa2c946c93e10782f4
yonas790 <yoni2752@gmail.com>
2025-05-05 15:26:26 +0300

    removing unwanted comments

--- ChatFX/src/main/java/com/yonas/chatfx/Connection/PropertiesClass.java
+++ ChatFX/src/main/java/com/yonas/chatfx/Connection/PropertiesClass.java
@@ -5,14 +5,7 @@ import java.util.Properties;
 
 public class PropertiesClass {
     public static final String FILE_NAME = "db.properties";
-    /**
-     * Method that reads the database properties file and returns the value associated with the specified field.
-     * Preconditions: the properties file exists and a non-null value is being searched for.
-     * Postconditions: returns the value associated with the specified field.
-     *
-     * @param field
-     * @return String
-     */
+
     public static String getProperty(String field) {
         String fieldValue;
         try (InputStream input = PropertiesClass.class.getClassLoader().getResourceAsStream("db.properties")) {
