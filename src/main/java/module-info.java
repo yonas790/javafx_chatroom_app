6f786d1e19261d3c35a20975c67154effb6f273c
yonas790 <yoni2752@gmail.com>
2025-05-10 19:41:19 +0300

    updating module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,12 @@
 module com.yonas.chat.chatroom_javafx {
-    
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.desktop;
+
+    requires java.xml.bind; // For JAXB (Java 9–10)
+    requires java.activation; // For JAXB's dependency
+    requires java.xml; // For JAXB's XML processing
+
     opens com.yonas.chat.chatroom_javafx to javafx.fxml;
     exports com.yonas.chat.chatroom_javafx;
 }
