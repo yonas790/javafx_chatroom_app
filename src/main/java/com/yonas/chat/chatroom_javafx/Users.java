3860212b97eb921424b0b813f2bfb54ff3cbf226
yonas790 <yoni2752@gmail.com>
2025-05-11 03:55:29 +0300

    adding users file class for displaying

---
+++ src/main/java/com/yonas/chat/chatroom_javafx/Users.java
@@ -0,0 +1,20 @@
+package com.yonas.chat.chatroom_javafx;
+
+import java.util.List;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "users")
+public class Users {
+	private List<User> users;
+
+	@XmlElement(name = "user")
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
